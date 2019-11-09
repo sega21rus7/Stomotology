2c641256c26119fb7c6d59d7a50cd8e1ab1f1e17
sergeev-av <sega21rus98@mail.ru>
2019-11-09 12:25:33 +0300

    Общий абстрактный класс для пациента и доктора

---
+++ Stomatology/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stomatology
+{
+    abstract class Person
+    {
+        public int id;
+        public int count;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+
+        public Person(string lastName, string firstName, string middleName)
+        {
+            this.id = this.count++;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.MiddleName = middleName;
+        }
+    }
+}
