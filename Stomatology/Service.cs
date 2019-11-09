50f0725af986bd4f3d585bf829625f24e8dc92d5
sergeev-av <sega21rus98@mail.ru>
2019-11-09 12:48:57 +0300

    Класс Услуга

--- Stomatology/Service.cs
+++ Stomatology/Service.cs
@@ -5,7 +5,24 @@ using System.Text;
 
 namespace Stomatology
 {
-    class Service
+    class Service : IPrimaryKey
     {
+        int pk;
+        int count;
+        public string Name { get; set; }
+        public string Price { get; set; }
+        //public string Type { get; set; }
+
+        public Service(string name, string price)
+        {
+            AssignPk();
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public void AssignPk()
+        {
+            pk = count++;
+        }
     }
 }
--- Stomatology/ServiceCost.cs
+++ Stomatology/ServiceCost.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Stomatology
 {
-    class ServiceCost
+    class ServiceCost : IPrimaryKey
     {
     }
 }
