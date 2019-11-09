a949d562e3bae8853f1228be259a36d1848287e9
sergeev-av <sega21rus98@mail.ru>
2019-11-09 13:35:31 +0300

    Класс Полис пациента

---
+++ Stomatology/PatientInsurancePolicy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stomatology
+{
+    class PatientInsurancePolicy : IPrimaryKey
+    {
+        int pk;
+        int count;
+        int series;
+        int number;
+
+        public PatientInsurancePolicy(int series, int number)
+        {
+            this.series = series;
+            this.number = number;
+        }
+
+        public void AssignPk()
+        {
+            pk = count++;
+        }
+    }
+}
---
+++ Stomatology/PatientPassport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stomatology
+{
+    class PatientPassport : IPrimaryKey
+    {
+        int pk;
+        int count;
+        int series;
+        int number;
+
+        public PatientPassport(int series, int number)
+        {
+            this.series = series;
+            this.number = number;
+        }
+
+        public void AssignPk()
+        {
+            pk = count++;
+        }
+    }
+}
