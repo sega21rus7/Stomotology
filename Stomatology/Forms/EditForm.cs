bd504106feeaed5fb7b509261ff26bdb8a1fffe3
sergeev-av <sega21rus98@mail.ru>
2019-11-24 10:09:56 +0300

    revert перенос базы

--- Stomatology/Forms/EditForm.cs
+++ Stomatology/Forms/EditForm.cs
@@ -15,11 +15,18 @@ namespace Stomatology.Forms
         private void button1_Click(object sender, System.EventArgs e)
         {
             var excelApp = new Excel.Application();
+<<<<<<< HEAD
             var workBook = excelApp.Workbooks.Open(Application.StartupPath + "\\Tables\\Patient.xlsx");
             var workSheet = workBook.Worksheets.get_Item(1); // первый лист
             var sheetRange = workSheet.UsedRange;// получаем все ячейки
             var rowsCount = sheetRange.Rows.Count;
             var columnsCount = sheetRange.Columns.Count;
+=======
+            var workBook = excelApp.Workbooks.Open("Patient.xlsx");
+            var workSheet = workBook.Worksheets["Лист1"];
+            
+            
+>>>>>>> parent of 54793bb... Перемещаем базу в bin/debug
             workBook.Close(false); //закрываем книгу, изменения не сохраняем
             excelApp.Quit(); //закрываем Excel
 
