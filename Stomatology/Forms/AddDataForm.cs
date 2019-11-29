8a397dd58e3e025fbf93774a9d32e8fa96f5d1e9
sergeev-av <sega21rus98@mail.ru>
2019-11-29 20:50:54 +0300

    Заготовки для формы добавления

--- Stomatology/Forms/AddDataForm.cs
+++ Stomatology/Forms/AddDataForm.cs
@@ -14,6 +14,21 @@ namespace Stomatology.Forms
         public AddDataForm()
         {
             InitializeComponent();
+
+            Label label = new Label();
+            label.Location = new Point(0, 10);
+            label.Text = "text";
+            label.TextAlign = ContentAlignment.MiddleCenter;
+
+            TextBox box = new TextBox();
+            box.Location = new Point(130, 10);
+
+            TextBox box2 = new TextBox();
+            box2.Location = new Point(130, 40);
+
+            this.Controls.Add(label);
+            this.Controls.Add(box);
+            this.Controls.Add(box2);
         }
     }
 }
