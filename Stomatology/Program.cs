fb09c9dd9517c8ef1f7f42d295244862468a2e76
sergeev-av <sega21rus98@mail.ru>
2019-11-29 20:55:32 +0300

    Мусор

--- Stomatology/Program.cs
+++ Stomatology/Program.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
-using Stomatology.Forms;
 
 namespace Stomatology
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
